249e3db8b81cd9b495c20e5939cba2bef8655b38
pjlanger1 <113559711+pjlanger1@users.noreply.github.com>
2023-12-18 14:56:01 -0500

    Update problem1b.py

--- problem1b.py
+++ problem1b.py
@@ -1,44 +1,24 @@
-def runcheck(a, b, visited):
-    list1 = []
-    directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
-    for dx, dy in directions:
-        try:
-            if final[a + dx][b + dy].isdigit() and (a + dx, b + dy) not in visited:
-                visited, pro = parsenum(a + dx, b + dy, visited)
-                list1.append(pro)
-        except IndexError:
-            pass
+diction = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
 
-    return visited, list1
-
-def parsenum(row, col, visited):
-    stage_row = final[row]
-    stage = []
-    flag = True
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-    if (row, col) in visited:
-        return visited, []
-
-    stage.append(stage_row[col])
-    visited.add((row, col))
-
-    for dx, dy in directions:
-        x, y = row, col
-        while 0 <= x + dx < final.shape[0] and 0 <= y + dy < final.shape[1] and final[x + dx][y + dy].isdigit():
-            x += dx
-            y += dy
-            stage.append(final[x][y])
-            visited.add((x, y))
-
-    return visited, ''.join(stage)
-
-nummies = []
-visited = set()
-
-for a in range(final.shape[0]):
-    for b in range(final.shape[1]):
-        if not final[a][b].isdigit() and final[a][b] != '.':
-            visited, fin = runcheck(a, b, visited)
-            nummies.append(fin)
+def parttwo(input):
+    d = []
+    #for each line in input string
+    for a in input:
+        #staging takes a list of parsed digits/written numbers in numeric
+        staging = []
+        #this is for building strings for comparison with the dictionary
+        stagestr = ''
+        for b in a:
+            if b.isdigit():
+                staging.append(str(b))
+                if stagestr != '':
+                    stagestr = ''
+            else:
+                stagestr += b
+                for k in diction.keys():
+                    if k in stagestr:
+                        staging.append(str(diction[k]))
+                        stagestr = '' + stagestr[-1]
+        d.append(staging[0]+staging[-1])
+    return d
