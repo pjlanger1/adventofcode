f013419b49c92ff5aac909c714cb9ba1bb460a79
pjlanger1 <113559711+pjlanger1@users.noreply.github.com>
2023-12-19 21:38:10 -0500

    Update problem2a.py

--- problem2a.py
+++ problem2a.py
@@ -1,18 +1,36 @@
-def parttwo(input):
-    d = []
-    for a in input:
-        staging = []
-        stagestr = ''
-        for b in a:
-            if b.isdigit():
-                staging.append(str(b))
-                if stagestr != '':
-                    stagestr = ''
-            else:
-                stagestr += b
-                for k in diction.keys():
-                    if k in stagestr:
-                        staging.append(str(diction[k]))
-                        stagestr = '' + stagestr[-1]
-        d.append(staging[0]+staging[-1])
-    return d
+#2A
+
+def processinput(r,g,b,inp):
+    validlist = []
+    for i in inp:
+        pattern = r'Game\s+(\d+):'
+        match = re.search(pattern, i)
+        if match:
+            print(i)
+            flag = True
+            #match.group(1)
+            for t in ['red','blue','green']:
+                d = max(tokenparse(i,t))
+                #print(t+" "+str(d)+" "+str(r))
+                #print(t+" "+str(d)+" "+str(b))
+                #print(t+" "+str(d)+" "+str(g))
+                if t == 'red':
+                    if d > r:
+                        flag = False
+                        break
+                if t == 'blue':
+                    if d > b:
+                        flag = False
+                        break
+                if t == 'green':
+                    if d > g:
+                        flag = False
+                        break
+            if flag:
+                validlist.append(int(match.group(1)))
+    return validlist
+
+def tokenparse(text,target_word):
+    pattern = rf'(\d+)\s*{re.escape(target_word)}'
+    matches = re.findall(pattern, text)
+    return [int(match) for match in matches]
